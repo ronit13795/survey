0c417b9450b744d0f8c7dc3eb3d89a892d2c3ca8
Ronit Cohen <ron13795@gmail.com>
2022-11-28 14:08:57 +0200

    add style

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,23 @@
 import dynamic from "next/dynamic";
+import { Fragment } from "react";
+import Link from "next/link";
+import style from "../styles/header.module.css";
 
 const DynamicForm = dynamic(() => import("../components/Form"), {
   ssr: false,
 });
 
 export default function Home() {
-  return <DynamicForm />;
+  return (
+    <Fragment>
+      <header className={style.header}>
+        <button>
+          <Link className={style.link} href={"/login"}>
+            ADMIN
+          </Link>
+        </button>
+      </header>
+      <DynamicForm />
+    </Fragment>
+  );
 }
