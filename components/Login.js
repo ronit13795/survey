82f850ffa98559b3f77cfa3215911cabbc25144c
Ronit Cohen <ron13795@gmail.com>
2022-11-28 14:27:09 +0200

    add check valid admin

--- components/Login.js
+++ components/Login.js
@@ -1,35 +1,62 @@
-import {useState} from 'react'
+import { useState } from "react";
+import { useRouter } from "next/router";
 
-export default function Login () {
+export default function Login() {
+  const [userName, setUserName] = useState("");
+  const [pw, setPw] = useState("");
+  const router = useRouter();
 
-  const [userName,setUserName] =useState('')
-  const [pw,setPw] =useState('')
-
-
-  const checkAdminCredentials = () =>{
-    
-
-  }
-
+  const checkAdminCredentials = () => {
+    fetch("/admin", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userName, password: pw }),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        if (json.success) {
+          return router.push("/Admin");
+        }
+        return alert("invalid credentials");
+      });
+  };
 
   return (
     <div className="login-container">
-
       <header>
         <h1>Admin Center</h1>
       </header>
 
       <div className="input-section">
-
-        <form onSubmit={(e)=>{e.preventDefault(); checkAdminCredentials()}} action="">
-          <input onChange={(e)=>{setUserName(e.target.value)}} type="text" placeholder="Username"/>
-          <input onChange={(e)=>{setPw(e.target.value)}} type="text" placeholder="Password"/>
-          <button type='submit'>Login</button> 
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            checkAdminCredentials();
+          }}
+          action=""
+        >
+          <input
+            onChange={(e) => {
+              setUserName(e.target.value);
+            }}
+            type="text"
+            placeholder="Username"
+          />
+          <input
+            onChange={(e) => {
+              setPw(e.target.value);
+            }}
+            type="text"
+            placeholder="Password"
+          />
+          <button type="submit">Login</button>
         </form>
-
       </div>
-      
     </div>
-  )
-
+  );
 }
