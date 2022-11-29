3d851033a1c2153ba1d260d5f981afe1373ba7f0
Ronit Cohen <ron13795@gmail.com>
2022-11-29 18:58:10 +0200

    bugs

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
-import '../styles/Login.css'
-import '../styles/Admin.css'
-import '../styles/Question.css'
+import "../styles/login.css";
+import "../styles/Admin.css";
+import "../styles/Question.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
