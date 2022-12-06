25f9531a82bd4a77cdef355907d62d4b8dc2e308
Aviv Asher <avivasher212@gmail.com>
2022-12-06 18:43:04 +0200

    style changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "../styles/login.css";
 import "../styles/Admin.css";
 import "../styles/Question.css";
+import "../styles/SidebarRight.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
