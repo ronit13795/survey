bd1626267843d0c5dc856f350d75e47b57c807c7
Aviv Asher <avivasher212@gmail.com>
2022-12-11 15:09:27 +0200

    Sign in and up

--- components/Login.js
+++ components/Login.js
@@ -79,7 +79,6 @@ export default function Login() {
           }}
         >
           <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign in
---
+++ pages/SignUp.js
@@ -0,0 +1,6 @@
+import SignUp from '../components/SignUp';
+
+export default function SignUpPage() {
+
+  return <SignUp />;
+}
