b5a580199ab54c08ef8fb139ac3a2f226797a683
Ronit Cohen <ron13795@gmail.com>
2022-12-18 13:05:36 +0200

    add password modal

--- pages/[surveyId]/index.js
+++ pages/[surveyId]/index.js
@@ -1,12 +1,27 @@
 import dbConnect from "../../lib/dbConnect";
 import surveyModel from "../../models/survey";
 import dynamic from "next/dynamic";
+import DialogPs from "../../components/DialogPs";
+import { useState } from "react";
 
 const DynamicForm = dynamic(() => import("../../components/Form"), {
   ssr: false,
 });
 
 export default function Survey({ survey }) {
+  const [userPassword, setUserPassword] = useState(null);
+  const [openModal, setOpenModal] = useState(!!survey.surveyPw);
+  const checkValidPassword = () => {
+    if (survey.surveyPw === userPassword) return setOpenModal(false);
+  };
+  if (survey.surveyPw && openModal) {
+    return (
+      <DialogPs
+        setUserPassword={setUserPassword}
+        checkValidPassword={checkValidPassword}
+      />
+    );
+  }
   return <DynamicForm survey={survey} />;
 }
 
