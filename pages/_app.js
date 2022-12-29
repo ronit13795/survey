54e760ca6d8b0a14f8a6300b496b038a69d7214b
rivkiRand <rivki6992@gmail.com>
2022-12-29 20:46:59 +0200

    app

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,8 @@ import "../styles/Admin.css";
 import "../styles/Question.css";
 import "../styles/SidebarRight.css";
 import "../styles/DropDown.css";
+import "../styles/MarketSurvey.css";
+import "../styles/Marketplace.css"
 import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
