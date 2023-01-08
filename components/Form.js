b0784a64eb4586716e8738e2cde2b6e318cde450
Ronit Cohen <ron13795@gmail.com>
2023-01-08 19:05:10 +0200

    styled components

--- components/Form.js
+++ components/Form.js
@@ -1,15 +1,21 @@
 import "survey-core/defaultV2.min.css";
 import { StylesManager, Model } from "survey-core";
 import { Survey } from "survey-react-ui";
-import { useCallback ,useState,useEffect} from "react";
+import { useCallback, useState, useEffect } from "react";
 import { style } from "@mui/system/Stack/createStack";
 import { blue, red } from "@mui/material/colors";
-
+import styled from "styled-components";
 
 StylesManager.applyTheme("defaultV2");
 
-export default function Form({ survey,setButton }) {
+export const StyledCSS = styled.div`
+  .myCustomClass {
+    color: ${(props) => props.colorBtnText};
+    background-color: ${(props) => props.backGroundBtn};
+  }
+`;
 
+export default function Form({ survey, setButton }) {
   const surveyToShow = new Model(survey);
   const alertResults = useCallback((sender) => {
     const results = JSON.stringify(sender.data);
@@ -22,7 +28,12 @@ export default function Form({ survey,setButton }) {
         Accept: "application/json",
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({title:surveyToShow.title,creator:survey.creator,surveyId:survey._id,answers:results}),
+      body: JSON.stringify({
+        title: surveyToShow.title,
+        creator: survey.creator,
+        surveyId: survey._id,
+        answers: results,
+      }),
     })
       .then((res) => {
         return res.json();
@@ -30,32 +41,47 @@ export default function Form({ survey,setButton }) {
       .then((json) => {
         if (json.success) {
           console.log("updated successfully");
-          setButton(true)
+          setButton(true);
         } else alert(json.msg);
       })
       .catch((err) => {
         alert("fatal error please try again latter");
       });
   }, []);
- console.log(survey);
+  console.log(survey);
 
-surveyToShow.onComplete.add(alertResults);
+  surveyToShow.onComplete.add(alertResults);
 
-useEffect(()=>{
-  document.getElementsByClassName("sd-container-modern")[0].style.backgroundColor = survey.background
-  document.getElementsByTagName("body")[0].style.backgroundColor = survey.background
-  document.getElementsByTagName("body")[0].style.color = survey.textColor
-  document.getElementsByTagName("body")[0].style.fontSize = survey.textSize
- document.getElementsByClassName("sv-string-viewer")[0].style.color = survey.titleColor
- document.getElementsByClassName("sv-string-viewer")[0].style.fontSize = survey.titleSize
-document.getElementsByClassName("sd-btn")[0].style.color = survey.btnColor
-document.getElementsByClassName("sd-btn")[0].style.backgroundColor = survey.btnBackground
-},[])
- 
+  useEffect(() => {
+    document.getElementsByClassName(
+      "sd-container-modern"
+    )[0].style.backgroundColor = survey.background;
+    document.getElementsByTagName("body")[0].style.backgroundColor =
+      survey.background;
+    document.getElementsByTagName("body")[0].style.color = survey.textColor;
+    document.getElementsByTagName("body")[0].style.fontSize = survey.textSize;
+    document.getElementsByClassName("sv-string-viewer")[0].style.color =
+      survey.titleColor;
+    document.getElementsByClassName("sv-string-viewer")[0].style.fontSize =
+      survey.titleSize;
+    document.getElementsByClassName("sd-btn")[0].style.color = survey.btnColor;
+    document.getElementsByClassName("sd-btn")[0].style.backgroundColor =
+      survey.btnBackground;
+  }, []);
+
+  const myCss = {
+    navigation: {
+      next: "myCustomClass",
+      complete: "myCustomClass",
+    },
+  };
 
   return (
-  <div>
-    <Survey model={surveyToShow} />
-    </div>
-  )
+    <StyledCSS
+      colorBtnText={survey.btnColor || "green"}
+      backGroundBtn={survey.backGroundBtn || "red"}
+    >
+      <Survey model={surveyToShow} css={myCss} />
+    </StyledCSS>
+  );
 }
